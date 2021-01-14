3aacbd8bcc82095eb98f289d0a00dad18c7c19a0
k0tark <48871496+k0tark@users.noreply.github.com>
2021-01-14 14:11:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ function Get(yourUrl){
     return Httpreq.responseText;          
 }
 var data = JSON.parse(Get("https://covid19.mathdro.id/api/countries/tn"));
-var confirmed=data['confirmed']["value"]
-var recovered=data['recovered']["value"]
-var deaths=data['deaths']["value"]
-document.getElementById("confirmed").innerText=confirmed
-document.getElementById("recovered").innerText=recovered
-document.getElementById("deaths").innerText=deaths
+var confirmed=data['confirmed']["value"];
+var recovered=data['recovered']["value"];
+var deaths=data['deaths']["value"];
+document.getElementById("confirmed").innerText=confirmed;
+document.getElementById("recovered").innerText=recovered;
+document.getElementById("deaths").innerText=deaths;
