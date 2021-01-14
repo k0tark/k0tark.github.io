e4dd115156112161d6c931d1fbb8fa7e3c534b4e
k0tark <48871496+k0tark@users.noreply.github.com>
2021-01-14 14:02:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+var data = JSON.parse(Get("https://covid19.mathdro.id/api/countries/tn"));
+var confirmed=data['confirmed']["value"]
+var recovered=data['recovered']["value"]
+var deaths=data['deaths']["value"]
+document.getElementById("confirmed").innerText=confirmed
+document.getElementById("recovered").innerText=recovered
+document.getElementById("deaths").innerText=deaths
