bfa410bd5bb472e8d61fd9471664cb7da6280249
k0tark <48871496+k0tark@users.noreply.github.com>
2021-01-18 18:58:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function Get(yourUrl){
     Httpreq.send(null);
     return JSON.parse(Httpreq.responseText);          
 }
-    var data = Get("https://covid19.mathdro.id/api/countries/tunisia");
-    var confirmed=data['confirmed']["value"]
-    var recovered=data['recovered']["value"]
-    var deaths=data['deaths']["value"]
-    document.getElementById("confirmed").innerText=confirmed
-    document.getElementById("recovered").innerText=recovered
-    document.getElementById("deaths").innerText=deaths
+var data = Get("https://covid19.mathdro.id/api/countries/tunisia");
+var confirmed=data['confirmed']["value"]
+var recovered=data['recovered']["value"]
+var deaths=data['deaths']["value"]
+document.getElementById("confirmed").innerText=confirmed
+document.getElementById("recovered").innerText=recovered
+document.getElementById("deaths").innerText=deaths
