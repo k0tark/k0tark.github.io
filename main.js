671899dd0dd51d00a5548ba2d47ee2f4bca0e1e9
k0tark <48871496+k0tark@users.noreply.github.com>
2021-01-18 02:29:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,10 @@ function Get(yourUrl){
     Httpreq.send(null);
     return JSON.parse(Httpreq.responseText);          
 }
-var requestUrl = "http://ip-api.com/json";
-$.ajax({
-  url: requestUrl,
-  type: 'GET',
-  success: function(json)
-  {
-    var data = Get("https://covid19.mathdro.id/api/countries/"+json.country);
-    document.getElementById("country").innerText=json.country
+    var data = Get("https://covid19.mathdro.id/api/countries/tunisia");
     var confirmed=data['confirmed']["value"]
     var recovered=data['recovered']["value"]
     var deaths=data['deaths']["value"]
     document.getElementById("confirmed").innerText=confirmed
     document.getElementById("recovered").innerText=recovered
-    document.getElementById("deaths").innerText=deaths  },
-  error: function(err)
-  {
-    alert("country not found in database!");
-  }
-});
+    document.getElementById("deaths").innerText=deaths
