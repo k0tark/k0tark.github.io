2823804b889f32bf7b08053e54370937fa2be0b0
k0tark <48871496+k0tark@users.noreply.github.com>
2021-01-18 02:19:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ function Get(yourUrl){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",yourUrl,false);
     Httpreq.send(null);
-    return Httpreq.responseText;          
+    return JSON.parse(Httpreq.responseText);          
 }
-var data = JSON.parse(Get("https://covid19.mathdro.id/api/countries/tn"));
-var confirmed=data['confirmed']["value"];
-var recovered=data['recovered']["value"];
-var deaths=data['deaths']["value"];
-document.getElementById("confirmed").innerText=confirmed;
-document.getElementById("recovered").innerText=recovered;
-document.getElementById("deaths").innerText=deaths;
+var requestUrl = "http://ip-api.com/json";
+$.ajax({
+  url: requestUrl,
+  type: 'GET',
+  success: function(json)
+  {
+    var data = Get("https://covid19.mathdro.id/api/countries/"+json.country);
+    document.getElementById("country").innerText=json.country
+    var confirmed=data['confirmed']["value"]
+    var recovered=data['recovered']["value"]
+    var deaths=data['deaths']["value"]
+    document.getElementById("confirmed").innerText=confirmed
+    document.getElementById("recovered").innerText=recovered
+    document.getElementById("deaths").innerText=deaths  },
+  error: function(err)
+  {
+    alert("country not found in database!");
+  }
+});
